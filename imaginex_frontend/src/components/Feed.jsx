aed5d5b6844d18749ec7eaea1717fb27723910f1
Paul <paul.iand3138@hotmail.com>
2023-07-03 19:53:56 +0800

    Complete fetching pin from sanity to frontend

--- imaginex_frontend/src/components/Feed.jsx
+++ imaginex_frontend/src/components/Feed.jsx
@@ -4,13 +4,34 @@ import { useParams } from "react-router-dom";
 import { client } from "../client";
 import MasonryLayout from "./MasonryLayout";
 import Spinner from "./Spinner";
+import { feedQuery, searchQuery } from "../utils/data";
 
 const Feed = () => {
     const [loading, setLoading] = useState(true);
+    const [pins, setPins] = useState(null);
+    const { categoryId } = useParams();
+
+    useEffect(() => {
+        setLoading(true);
+
+        if (categoryId) {
+            const query = searchQuery(categoryId);
+
+            client.fetch(query).then((data) => {
+                setPins(data);
+                setLoading(false);
+            });
+        } else {
+            client.fetch(feedQuery).then((data) => {
+                setPins(data);
+                setLoading(false);
+            });
+        }
+    }, [categoryId]);
 
     if (loading)
         return <Spinner message="We are fetching new ideas to your feed!" />;
-    return <div>Feed</div>;
+    return <div>{pins && <MasonryLayout pins={pins} />}</div>;
 };
 
 export default Feed;
---
+++ imaginex_frontend/src/components/Pin.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { urlFor } from "../client";
+
+const Pin = ({ pin: { postedBy, image, _id, destination } }) => {
+    return (
+        <div>
+            {image && (
+                <img
+                    className="rounded-lg w-full"
+                    alt="user-post"
+                    src={urlFor(image).width(250).url()}
+                />
+            )}
+        </div>
+    );
+};
+
+export default Pin;
